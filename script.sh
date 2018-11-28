e8b31d87c1afb01e91b5e1dff42e7ce0261a7f99
Jenkins <jenkins@ip-172-31-33-228.us-east-2.compute.internal>
2018-11-28 12:36:58 +0000

    updated

--- script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-var1=$(sed -n '/<packaging>jar<\/packaging>/q;p' ./pom.xml | sed '0,/^<artifactId>my-application1<\/artifactId>$/d' | grep -oPm1 "(?<=<version>)[^<]+")
-orig_val=$var1
-var1=$(echo "$var1 + 1" | bc -l)
-cat pom.xml | tr '\n' '\r' | sed "s/$orig_val<\/version>\r<packaging>/$var1<\/version>\r<packaging>/g" | tr '\r' '\n' | head
