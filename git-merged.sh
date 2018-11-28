b94a2b86b34b75eba4cf532410707d9737b63882
EC2 Default User <ec2-user@ip-172-31-28-195.eu-central-1.compute.internal>
2018-11-28 08:57:56 +0000

    llll

---
+++ git-merged.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+remote="${1:-origin}"
+branch="$2"
+git branch -r --list "$remote/*" --merged $branch \
+    | sed "s/\s*$remote\///" \
+    | egrep -v "^(HEAD|release|hotfix|master|develop)"
