511fb87651e3e37bbe72ceffde3924406806774c
EC2 Default User <ec2-user@ip-172-31-28-195.eu-central-1.compute.internal>
2018-12-04 11:12:51 +0000

    nd

--- mainscript.sh
+++ mainscript.sh
@@ -1,8 +1,16 @@
 #/bin/bash/
 
+git_merged(){
+remote="${1:-origin}"
+branch="$2"
+git branch -r --list "$remote/*" --merged $branch \
+    | sed "s/\s*$remote\///" \
+    | egrep -v "^(HEAD|release|hotfix|master|develop)"
+}
+
 or=remotes/origin/
-for k in $(./git-merged.sh origin develop | grep feature); do 
-  if [ -n "$(git log -1 --since='10 week ago' -s $or$k)" ]; then
+for k in $(git_merged origin develop ); do 
+  if [ -n "$(git log -1 --before='28 Nov 2018'  -s $or$k)" ]; then
     #git push t push origin --delete -norigin --delete -n $k
   c=$k
   echo $or$c
