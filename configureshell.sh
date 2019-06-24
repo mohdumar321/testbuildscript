2f39ec5e0efdf41dff4a0d3e743a10302e07845e
Mohd Umar Mubeen <umar.shiats@gmail.com>
2019-06-24 12:46:07 +0530

    COMM

---
+++ configureshell.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo ARTIFACT_ID=${ARTIFACT_ID}
+echo IMAGE=docker-registry.default.svc:5000/${NAMESPACE}/${ARTIFACT_ID}:$TAG
+echo HEALTH_CHECK_URL=${HEALTH_CHECK_URL} 
+echo ROUTE=${ARTIFACT_ID}-${NAMESPACE}.bss-np.corp.cox.com
