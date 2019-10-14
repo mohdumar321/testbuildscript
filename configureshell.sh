a1c2209bc557b31782bd5cffc2909253bba08970
Mohd Umar Mubeen <umar.shiats@gmail.com>
2019-10-14 20:17:57 +0530

    Update configureshell.sh

--- configureshell.sh
+++ configureshell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo ARTIFACT_ID=${ARTIFACT_ID}
-echo IMAGE=docker-registry.default.svc:5000/${NAMESPACE}/${ARTIFACT_ID}:$TAG
+echo IMAGE=docker-reg.def.svc:5000/${NAMESPACE}/${ARTIFACT_ID}:$TAG
 echo HEALTH_CHECK_URL=${HEALTH_CHECK_URL} 
-echo ROUTE=${ARTIFACT_ID}-${NAMESPACE}.svss-nop.corp.example.com
+echo ROUTE=${ARTIFACT_ID}-${NAMESPACE}.svss-nop.cO.example.com
